5a3caafad9e61c7e7ccd58b6e6c2f86406f77928
JeffreyTerry <jterry94@gmail.com>
2014-03-05 20:47:32 -0600

    more lot improvements

--- public/js/views/sellParking/sellParking.js
+++ public/js/views/sellParking/sellParking.js
@@ -11,8 +11,9 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/sellParking/sellPark
       },
 
       initialize: function() {
-        this.listenTo(this.collection, 'change', this.renderLotView);
-        this.listenTo(this.collection, 'destroy', this.remove);
+        this.listenTo(this.collection, 'add', this.renderLotView);
+        this.listenTo(this.collection, 'remove', this.renderLotView);
+        this.listenTo(this.collection, 'reset', this.render);
       },
 
       render: function() {
@@ -20,8 +21,8 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/sellParking/sellPark
         var lotListView = new LotListView( {model: this.model, collection: this.collection} );
         this.$el.find('#lot-list-view-container').html( lotListView.render().el );
         if(this.collection.length > 0){
-          var lotView = new LotView( {model: this.collection.at(0), collection: this.model} );
-          this.$el.find('#lot-view-container').html( lotView.render().el );
+          this.lotView = new LotView( {model: this.collection.at(0), collection: this.model} );
+          this.$el.find('#lot-view-container').html( this.lotView.render().el );
         }
         return this;
       },
@@ -29,11 +30,20 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/sellParking/sellPark
       renderLotView: function(event){
         if(event.currentTarget){
           var lotId = event.currentTarget.id.slice(event.currentTarget.id.lastIndexOf('-') + 1, event.currentTarget.id.length);
-          var lotView = new LotView( {model: this.collection.get(lotId), collection: this.model} );
-          this.$el.find('#lot-view-container').html( lotView.render().el );
+          this.lotView = new LotView( {model: this.collection.get(lotId), collection: this.model} );
+          this.$el.find('#lot-view-container').html( this.lotView.render().el );
         } else{
-          var lotView = new LotView( {model: this.collection.at(0), collection: this.model} );
-          this.$el.find('#lot-view-container').html( lotView.render().el );
+          if(!this.lotView){
+            this.lotView = new LotView( {model: this.collection.at(0), collection: this.model} );
+            this.$el.find('#lot-view-container').html( this.lotView.render().el );
+            return;
+          }
+          var currentLotSelected = this.collection.get(this.lotView.model.get('_id'));
+          console.log(currentLotSelected);
+          if(!currentLotSelected){
+            this.lotView = new LotView( {model: this.collection.at(0), collection: this.model} );
+            this.$el.find('#lot-view-container').html( this.lotView.render().el );
+          }
         }
       }
     });
