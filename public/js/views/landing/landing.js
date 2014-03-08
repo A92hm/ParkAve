373530363802f0086fc903041aa0527eccdba48d
JeffreyTerry <jterry94@gmail.com>
2014-03-08 14:51:44 -0600

    fixed landing css

--- public/js/views/landing/landing.js
+++ public/js/views/landing/landing.js
@@ -1,11 +1,10 @@
-define(['jquery', 'underscore', 'backbone', 'text!templates/landing/landing.html',
-        'views/landing/login', 'models/session',
-        'collections/sessions', 'routing/router'],
-  function($, _, Backbone, Template, LoginView, Session, SessionsCollection, Router) {
+define(['jquery', 'underscore', 'backbone', 'stellar', 'text!templates/landing/landing.html',
+        'models/session', 'collections/sessions', 'routing/router'],
+  function($, _, Backbone, stellar, Template, Session, SessionsCollection, Router) {
 
   var LandingView = Backbone.View.extend({
     tagName: 'div',
-    className: 'landing-body-div',
+    className: 'landing-div',
     template: _.template( Template ),
 
     events: {
@@ -14,54 +13,10 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/landing/landing.html
     },
 
     initialize: function() {
-        // var parallaxScroll,
-        // _this = this;
-
-        // parallaxScroll = function() {
-        //   var currentScrollPosition;
-        //   currentScrollPosition = $(_this).scrollTop();
-        //   $('.opening').css({
-        //     'background-position': '50% ' + (-currentScrollPosition / 4) + 'px'
-        //   });
-        //   return $('.openingText').css({
-        //     'margin-top': (currentScrollPosition / 4) + "px",
-        //     'opacity': 1 - (currentScrollPosition / 250)
-        //   });
-        // };
-
-        // $(document).ready(function() {
-        //   $(window).scroll(function() {
-        //     return parallaxScroll();
-        //   });
-        //   console.log($(window).scroll);
-        //   return $(document).scroll(function() {
-        //     var bottomOfOpening, compareWindowHeight, header, sideBar, windowHeight, windowTop;
-        //     windowTop = $(window).scrollTop();
-        //     bottomOfOpening = $('.opening').position().top + $('.opening').height();
-        //     header = $('.header');
-        //     sideBar = $('.sideBar');
-        //     windowHeight = $(window).height();
-        //     compareWindowHeight = (windowHeight - 70) + "px";
-        //     if (bottomOfOpening > windowTop) {
-        //       return header.css({
-        //         'position': 'absolute',
-        //         'top': '100%',
-        //         'left': '0'
-        //       });
-        //     } else {
-        //       return header.css({
-        //         'position': 'fixed',
-        //         'top': '0',
-        //         'left': '0'
-        //       });
-        //     }
-        //   });
-        // });
     },
 
     render: function() {
       this.$el.html( this.template() );
-
       return this;
     },
 
