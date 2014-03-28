386ba5bbada82d5c8625d84e8a76d8e498963533
epanjei6032 <egawati.panjei@gmail.com>
2014-03-28 16:21:07 -0500

    add uploader

--- public/js/views/imageUploader/imageuploader.js
+++ public/js/views/imageUploader/imageuploader.js
@@ -1,8 +1,10 @@
 define(['jquery', 'underscore', 'backbone', 'text!templates/user/imageuploader.html',
         'models/user', 'models/session', 'collections/users',
         'collections/sessions', 'routing/router', 'assets/imageuploader/javascripts/jquery/ui.widget',
-        'assets/imageuploader/javascripts/jquery/fileupload', 'assets/imageuploader/javascripts/jquery/app'],
-  function($, _, Backbone, Template, User, Session, UsersCollection, SessionsCollection, Router, Widget, FileUploader, S3Uploader) {
+        'assets/imageuploader/javascripts/jquery/fileupload', 'assets/imageuploader/javascripts/jquery/app', 
+        'collections/s3Collection'],
+  function($, _, Backbone, Template, User, Session, UsersCollection, SessionsCollection, Router, 
+    Widget, FileUploader, S3Uploader, S3Collection) {
 
   var ImageUploaderView = Backbone.View.extend({
     tagName: 'div',
