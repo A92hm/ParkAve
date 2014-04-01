2c9da0617a0db9995ea48f9cf960d1f80add3131
epanjei6032 <egawati.panjei@gmail.com>
2014-04-01 12:37:53 -0500

    image

--- public/js/views/application/main.js
+++ public/js/views/application/main.js
@@ -265,11 +265,17 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/application/main.htm
     },
     
     showImageUploader: function() {
-      var thisGuy = this;
-      var s3Collection = new S3Collection();
-      var imageUploderView = new ImageUploaderView({collection:s3Collection});
-      thisGuy.$el.html(imageUploderView.render().el);
-      s3Collection.fetch();
+      //var thisGuy = this;
+      //var s3Collection = new S3Collection();
+      //var imageUploderView = new ImageUploaderView({collection:s3Collection});
+      //thisGuy.$el.html(imageUploderView.render().el);
+      //s3Collection.fetch();
+      var imageUploaderView = new ImageUploaderView();
+      this.$el.html( landingView.render().el );
+      require(['stellar'], function(stellar) {
+        $.stellar();
+        $.stellar('refresh');
+      });
     }
   });
 
