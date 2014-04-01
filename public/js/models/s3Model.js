0fbc3feac52ed862781d24e353cdc1f41fc112e1
epanjei6032 <egawati.panjei@gmail.com>
2014-04-01 15:51:58 -0500

    image

--- public/js/models/s3Model.js
+++ public/js/models/s3Model.js
@@ -3,7 +3,11 @@ define(['underscore','backbone'], function(_, Backbone) {
   var S3Model = Backbone.Model.extend({
     idAttribute: '_id',
     defaults: {
-      
+    	policy: '',
+        signature: '',
+        key: '',
+        success_action_redirect: '',
+        contentType: ''
     },
     initialize: function() {
     }
