382e7c2cfe3f6ede3afc58ce8c07aef85510ee81
epanjei6032 <egawati.panjei@gmail.com>
2014-04-02 18:24:06 -0500

    new uploader

--- public/js/utility/s3upload.js
+++ public/js/utility/s3upload.js
@@ -12,11 +12,11 @@ Unless required by applicable law or agreed to in writing, software distributed 
 
 */
 
-define([],function() {
+(function() {
 
   window.S3Upload = (function() {
 
-    S3Upload.prototype.s3_sign_put_url = '/api/signS3put';
+    S3Upload.prototype.s3_sign_put_url = '/signS3put';
 
     S3Upload.prototype.file_dom_selector = '#file_upload';
 
@@ -142,6 +142,8 @@ define([],function() {
       }, opts);
     };
 
-  })
-return S3Upload;
-})
+    return S3Upload;
+
+  })();
+
+}).call(this);
