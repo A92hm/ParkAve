b44e4b519e3b02565903f240a56947ee41d67d79
Jeremy Littel <jeremy.littel@gmail.com>
2014-04-01 20:55:13 -0500

    got the site to display the bucket

--- public/js/views/imageUploader/imageuploader.js
+++ public/js/views/imageUploader/imageuploader.js
@@ -12,21 +12,39 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/imageUploader/imageu
     },
 
     initialize:function () {
-      var s3Model = new S3Model();
-      var collection = new S3Collection([s3Model]);
-      var test = collection.toJSON();
+      this.s3Model = new S3Model();
+      this.collection = new S3Collection([this.s3Model]);
+      //collection.fetch();
+      //var test = collection.toJSON();
       console.log('test');
-      console.log(s3Model.fetch());
+      //console.log(s3Model.fetch());
+      var theView = this;
       this.collection.fetch({
         error:function(){
           console.log(error);
+        },
+        success:function(){
+          console.log('success!');
+          //console.log(this);
+          theView.render();
         }
       });
-      this.render();
+
+
+      //this.render();
+      this.listenTo(this.collection, 'change', this.render);
+      //this.listenTo(this.user, 'destroy', this.remove);
+
     },
 
     render: function() {
-      this.$el.html( this.template({test:this.test}));
+      if (this.collection.length > 0) {
+        this.s3Model = this.collection.at(0);
+      }
+      console.log('aws: '+this.s3Model.get('aws_bucket'));
+      console.log(this.s3Model);
+      //this.s3Model.set('aws_bucket', 'this is a test');
+      this.$el.html( this.template( this.s3Model.toJSON() ));
       return this;
     },
 
