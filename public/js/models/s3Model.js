e3bc2a399674b4954623881368fc207a1100e271
epanjei6032 <egawati.panjei@gmail.com>
2014-04-01 19:22:59 -0500

    image

--- public/js/models/s3Model.js
+++ public/js/models/s3Model.js
@@ -13,7 +13,7 @@ define(['underscore','backbone'], function(_, Backbone) {
       contentType: ''
     },
     parse:function (response) {
-      //console.log(response);
+      console.log(response);
       response.id = response._id;
       return response;
     }
