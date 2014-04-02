3fadb346caa1a92dc1f5d6d539b0fbb9f5b0520a
epanjei6032 <egawati.panjei@gmail.com>
2014-04-02 16:13:02 -0500

    new uploader

--- public/js/views/imageUploader/uploaderview.js
+++ public/js/views/imageUploader/uploaderview.js
@@ -16,7 +16,6 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/imageUploader/upload
 
     render: function() {
       this.$el.html(this.template());
-      return this;
     },
 
     uploadFileToS3 : function (){
