cca25a8f4ea09c45d651bfbc5d41c03a3f416c3b
epanjei6032 <egawati.panjei@gmail.com>
2014-04-03 17:51:28 -0500

    help uploader

--- public/js/utility/s3upload.js
+++ public/js/utility/s3upload.js
@@ -1,18 +1,4 @@
-/*
-
-Copyright 2013 tadruj
-
-Project home: https://github.com/tadruj/s3upload-coffee-javascript
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-
-*/
-
-define([], function() {
+(function() {
 
   window.S3Upload = (function() {
 
@@ -130,7 +116,9 @@ define([], function() {
         return this_s3upload.uploadToS3(file, signedURL, publicURL);
       });
     };
-  });
-  return S3Upload;
 
-})
+    return S3Upload;
+
+  })();
+
+}).call(this);
