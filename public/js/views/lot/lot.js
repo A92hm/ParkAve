7ae47072b6e83f2003fa60baae700ee7d2fc85bb
Ali Hajimirza <a92hm@yahoo.com>
2014-04-17 22:48:14 -0500

    street view

--- public/js/views/lot/lot.js
+++ public/js/views/lot/lot.js
@@ -31,10 +31,10 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/lot/lot.html', 'text
         var theLat = this.model.get('lat');
         var theLon = this.model.get('lon');
         console.log(theLat);
-        theLat = theLat.toFixed(5);
-        theLon = theLon.toFixed(5);
+        theLat = theLat.toFixed(3);
+        theLon = theLon.toFixed(3);
         console.log(theLat);
-        var attr = "http://maps.googleapis.com/maps/api/streetview?size=850x300&location="+theLat+","+theLon+"&sensor=false";
+        var attr = "http://maps.googleapis.com/maps/api/streetview?size=850x300&location="+theLat+","+theLon+"&fov=90&heading=235&pitch=10&sensor=false";
         this.$el.html( this.template( this.model.toJSON() ) );
         this.$el.find('.lot-page-main-image').attr('src',attr);
         console.log(this.$el);
