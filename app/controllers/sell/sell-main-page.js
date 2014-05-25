8fb95920aa588d5a42836122a41aa7e49db7223f
JeffreyTerry <jterry94@gmail.com>
2014-05-25 17:55:42 -0500

    added routing stuff

---
+++ app/controllers/sell/sell-main-page.js
@@ -0,0 +1,5 @@
+exports.index = function(req, res){
+  res.render('sell/sell-main-page', {
+    title: 'Park Ave'
+  });
+};
--- config/routes.js
+++ config/routes.js
@@ -1,18 +1,34 @@
 var passport = require('passport')
-	, LocalStrategy = require('passport-local').Strategy
-	, home = require('../app/controllers/home')
-	, user = require('../app/controllers/users')
-	, session = require('../app/controllers/session');
+  , LocalStrategy = require('passport-local').Strategy
+  , _ = require('underscore')
+  , buy_main_page = require('../app/controllers/buy/buy-main-page')
+  , sell_main_page = require('../app/controllers/sell/sell-main-page')
+  , user = require('../app/controllers/users')
+  , session = require('../app/controllers/session');
 
+// Stores a dictionary with route paths as keys and their corresponding static html files as values.
+var URLToStaticFileMap = {
+  '/': 'home/home'
+};
+
+var renderStaticPage = function(req, res){
+  res.render(URLToStaticFileMap[req.route.path], {
+      title: 'Park Ave'
+  });
+};
 
 module.exports = function(app, io){
+/* API Routes */
 
-	
-	app.get('/', home.index);
+/* User Routes */
+  // Static Pages
+  _.each(URLToStaticFileMap, function(value, key){
+    app.get(key, renderStaticPage);
+  });
 
-
-	app.post('/login', session.postlogin);
-
-	app.get('/logout', session.logout);
-
+  // Other Routes
+  app.get( '/buyparking', buy_main_page.index);
+  app.get( '/sellparking', sell_main_page.index);
+  app.post('/login', session.postlogin);
+  app.get( '/logout', session.logout);
 };
