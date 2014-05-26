52ee9ec2f9fdf5f7db14aa22ffc1e3a6fbed76d0
JeffreyTerry <jterry94@gmail.com>
2014-05-25 19:07:15 -0500

    added test folder

---
+++ tests/status_code_checks.js
@@ -0,0 +1,6 @@
+var https = require('https')
+  , vows = require('vows')
+  , assert = require('assert')
+  , buy_main_page = require('./../app/controllers/buy/buy-main-page.js')
+  , sell_main_page = require('./../app/controllers/sell/sell-main-page.js');
+  
